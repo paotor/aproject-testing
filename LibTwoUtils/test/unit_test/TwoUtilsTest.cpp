ce7cfa3494d1e5f39e19c619753f7bc759016cd5
Paolo Torrente <paolo.torrente@ericsson.com>
2018-03-07 11:36:58 +0100

    Inital .travis.yml configuration

--- LibTwoUtils/test/unit_test/TwoUtilsTest.cpp
+++ LibTwoUtils/test/unit_test/TwoUtilsTest.cpp
@@ -15,15 +15,16 @@ namespace testing {
     TEST(TwoUtilsTest, checkFindMaxIsFiveDotZero) {
         double myDoubles[] = { 1.33, 4.9, 5.0, -3.2, 0 };
         std::vector<double> samples(myDoubles, myDoubles + sizeof(myDoubles) / sizeof(double));
+//        std::vector<double> samples { 1.33e+0, 4.9e+0, 5.0e+0, -3.2e+0, 0 };
         
-        EXPECT_EQ(LibTwoUtils::find_max(samples), 5.0) << "Check samples's max is 5.0 " ;
+        EXPECT_DOUBLE_EQ(LibTwoUtils::find_max(samples), 5.0e+0) << "Check samples's max is 5.0 " ;
     }
     
     TEST(TwoUtilsTest, checkMeanIsOnePointTwentySix) {
         double myDoubles[] = { 1.3, 3.2, 5.0, -3.2, 0 };
         std::vector<double> samples(myDoubles, myDoubles + sizeof(myDoubles) / sizeof(double));
         
-        EXPECT_EQ(LibTwoUtils::sample_mean(samples), 1.26) << "Check samples's mean is 1.26 " ;
+        EXPECT_DOUBLE_EQ(LibTwoUtils::sample_mean(samples), 1.26) << "Check samples's mean is 1.26 " ;
     }
 
     TEST(TwoUtilsTest, checkMeanIsLessEqualThanMax) {
@@ -39,7 +40,7 @@ namespace testing {
         std::vector<double> samples(myDoubles, myDoubles + sizeof(myDoubles) / sizeof(double));
             
         ASSERT_LE(LibTwoUtils::sample_mean(samples),
-                  LibTwoUtils::find_max(samples)) << "Check sample_mean and find_max manage one item's vector " ;
+                  LibTwoUtils::find_max(samples)) << "Check sample_mean and find_max managing one item's vector " ;
     }
     
    
